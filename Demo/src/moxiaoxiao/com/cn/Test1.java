c5ae5213396b4dfd761729410435e2c77f108ae9
xuejing <xuejing@yunzongnet.com>
2020-05-27 11:33:00 +0800

    测试git提交

---
+++ Demo/src/moxiaoxiao/com/cn/Test1.java
@@ -0,0 +1,13 @@
+package moxiaoxiao.com.cn;
+
+/**
+ * @program: TestJava
+ * @description:
+ * @author: XueJing
+ * @create: 2020-05-27 11:21:25
+ **/
+public class Test1 {
+    public static void main(String args[]){
+        System.out.println("=============" + "test git commit");
+    }
+}
